aea4fc60706dbf43bb7c31ff76a5516d982dc68d
Alain Descartes <ualaindescartes@gmail.com>
2025-05-13 11:23:58 -0600

    updated task interface

--- models/interfaces/task.interface.ts
+++ models/interfaces/task.interface.ts
@@ -1,13 +1,13 @@
 export interface Task {
   id: number;
-  taskType: string;
-  assignedToStaffId: number;
-  residentId?: number; // Nullable if not assigned to a specific resident
-  taskStatus: "pending" | "in_progress" | "completed";
-  completionTime?: string; // ISO string (nullable)
-  taskPriority: "low" | "medium" | "high";
-  createdAt: string; // ISO string
-  updatedAt: string; // ISO string
+  description: string;
+  status: 'pending' | 'completed';
+  groupHomeId: number;
+  residentId?: number;
+  completedAt?: string;
+  completedBy: number;
+  createdAt: string;
+  updatedAt: string;
 }
 
-export type TaskInsert = Omit<Task, "id" | "createdAt" | "updatedAt">;
+export type TaskInsert = Omit<Task, 'id' | 'createdAt' | 'updatedAt' | 'completedBy'>;
--- routes/taskRoutes.ts
+++ routes/taskRoutes.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import checkAuth, { checkRole } from "../utils/checkAuth.js";
-import { addTaskData } from "../controller/taskController.js";
+import express from 'express';
+import checkAuth, { checkRole } from '../utils/checkAuth.js';
+import { addTaskData } from '../controller/taskController.js';
 
 const router = express.Router();
 
-router.post("/add-task", checkAuth, checkRole, addTaskData);
+router.post('/add-task', checkAuth, checkRole, addTaskData);
 
 export default router;
