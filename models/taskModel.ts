d378e6a1f5dc091a6ad1546e8cf84b34a789f59f
Alain Descartes <ualaindescartes@gmail.com>
2025-05-13 15:40:56 -0600

    added task route

--- models/taskModel.ts
+++ models/taskModel.ts
@@ -1,10 +1,26 @@
-import { Knex } from "knex";
-import { TaskInsert } from "./interfaces/task.interface.js";
+import { Knex } from 'knex';
+import { TaskInsert, Task } from './interfaces/task.interface.js';
 
-export async function addTask(
-  taskData: TaskInsert | TaskInsert[],
-  knex: Knex
-): Promise<TaskInsert[]> {
-  const rows = await knex("tasks").insert(taskData).returning("*");
+export async function addTask(taskData: TaskInsert | TaskInsert[], knex: Knex): Promise<Task[]> {
+  const rows = await knex('tasks').insert(taskData).returning('*');
   return rows;
+}
+
+export async function findTaskById(taskId: number, knex: Knex): Promise<Task> {
+  const task = await knex('tasks').where({ id: taskId }).first();
+
+  if (!task) {
+    throw new Error(`Task with id ${taskId} not found`);
+  }
+  return task;
+}
+
+export async function editTask(id: number, taskData: TaskInsert, knex: Knex): Promise<Task> {
+  const updated = await knex('tasks').where({ id }).update(taskData).returning('*');
+
+  if (updated.length === 0) {
+    throw new Error(`Task with id ${id} not found`);
+  }
+
+  return updated[0];
 }
--- routes/taskRoutes.ts
+++ routes/taskRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import checkAuth, { checkRole } from '../utils/checkAuth.js';
-import { addTaskData } from '../controller/taskController.js';
+import { addTaskData, editTaskData, markTaskAsCompleted } from '../controller/taskController.js';
 
 const router = express.Router();
 
 router.post('/add-task', checkAuth, checkRole, addTaskData);
+router.post('/edit-task/:id', checkAuth, editTaskData);
 
 export default router;
