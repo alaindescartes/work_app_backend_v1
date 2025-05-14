8a5dbae2f2fb248931a12c0426370f78757d40ef
alain descartes <ualaindescartes@gmail.com>
2025-05-14 16:04:20 -0600

    added method to get task by  homeid

--- models/taskModel.ts
+++ models/taskModel.ts
@@ -1,13 +1,16 @@
-import { Knex } from 'knex';
-import { TaskInsert, Task } from './interfaces/task.interface.js';
+import { Knex } from "knex";
+import { TaskInsert, Task } from "./interfaces/task.interface.js";
 
-export async function addTask(taskData: TaskInsert | TaskInsert[], knex: Knex): Promise<Task[]> {
-  const rows = await knex('tasks').insert(taskData).returning('*');
+export async function addTask(
+  taskData: TaskInsert | TaskInsert[],
+  knex: Knex
+): Promise<Task[]> {
+  const rows = await knex("tasks").insert(taskData).returning("*");
   return rows;
 }
 
 export async function findTaskById(taskId: number, knex: Knex): Promise<Task> {
-  const task = await knex('tasks').where({ id: taskId }).first();
+  const task = await knex("tasks").where({ id: taskId }).first();
 
   if (!task) {
     throw new Error(`Task with id ${taskId} not found`);
@@ -15,8 +18,27 @@ export async function findTaskById(taskId: number, knex: Knex): Promise<Task> {
   return task;
 }
 
-export async function editTask(id: number, taskData: TaskInsert, knex: Knex): Promise<Task> {
-  const updated = await knex('tasks').where({ id }).update(taskData).returning('*');
+export async function findTaskByGroupHome(
+  homeId: number,
+  knex: Knex
+): Promise<Task[]> {
+  if (!homeId) {
+    throw new Error(`GroupHome ID is required`);
+  }
+
+  const tasks = await knex("tasks").where({ groupHomeId: homeId });
+  return tasks;
+}
+
+export async function editTask(
+  id: number,
+  taskData: TaskInsert,
+  knex: Knex
+): Promise<Task> {
+  const updated = await knex("tasks")
+    .where({ id })
+    .update(taskData)
+    .returning("*");
 
   if (updated.length === 0) {
     throw new Error(`Task with id ${id} not found`);
