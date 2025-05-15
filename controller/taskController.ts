18b6fe27a05058d43aebac57d3ad1a4f1962ef4b
alain descartes <ualaindescartes@gmail.com>
2025-05-14 23:28:02 -0600

    added edit logic

--- controller/taskController.ts
+++ controller/taskController.ts
@@ -1,7 +1,12 @@
-import { Request, Response, NextFunction } from 'express';
-import { addTask, editTask, findTaskById } from '../models/taskModel.js';
-import { AppError } from '../utils/appError.js';
-import { TaskInsert } from '../models/interfaces/task.interface.js';
+import { Request, Response, NextFunction } from "express";
+import {
+  addTask,
+  editTask,
+  findTaskByGroupHome,
+  findTaskById,
+} from "../models/taskModel.js";
+import { AppError } from "../utils/appError.js";
+import { Task, TaskInsert } from "../models/interfaces/task.interface.js";
 
 /**
  * Controller to handle task creation requests.
@@ -14,23 +19,31 @@ import { TaskInsert } from '../models/interfaces/task.interface.js';
  *
  * @returns {Promise<void>} Sends a JSON response on success or calls `next` with an error.
  */
-export async function addTaskData(req: Request, res: Response, next: NextFunction) {
+export async function addTaskData(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const taskData: TaskInsert | TaskInsert[] = req.body;
   if (!taskData || (Array.isArray(taskData) && taskData.length === 0)) {
-    return next(new AppError('Task data is required', 400));
+    return next(new AppError("Task data is required", 400));
   }
 
   try {
     //insert task in the db
-    const addedtask = await addTask(taskData, req.app.get('db'));
+    const addedtask = await addTask(taskData, req.app.get("db"));
 
     //added task successfully
-    res.status(200).json({ message: 'Tasks added successfully', tasks: addedtask });
+    res
+      .status(200)
+      .json({ message: "Tasks added successfully", tasks: addedtask });
   } catch (error: any) {
-    if (process.env.NODE_ENV !== 'production') {
-      console.log('unable to insert task', error.message);
+    if (process.env.NODE_ENV !== "production") {
+      console.log("unable to insert task", error.message);
     }
-    return next(new AppError(error.message || 'an error occured while adding tasks', 500));
+    return next(
+      new AppError(error.message || "an error occured while adding tasks", 500)
+    );
     //TODO:log error to a logging system
   }
 }
@@ -47,38 +60,86 @@ export async function addTaskData(req: Request, res: Response, next: NextFunctio
  *
  * @returns {Promise<void>} Sends a JSON response with the updated task or forwards an error to middleware.
  */
-export async function editTaskData(req: Request, res: Response, next: NextFunction) {
+export async function editTaskData(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const taskData: TaskInsert = req.body;
   const { id } = req.params;
 
   const taskId = Number(id);
   if (isNaN(taskId)) {
-    return next(new AppError('Invalid task ID', 400));
+    return next(new AppError("Invalid task ID", 400));
   }
 
   if (!id) {
-    return next(new AppError('task id must be provided', 400));
+    return next(new AppError("task id must be provided", 400));
   }
 
   try {
-    const taskToEdit = await findTaskById(taskId, req.app.get('db'));
+    const taskToEdit = await findTaskById(taskId, req.app.get("db"));
 
     //task not found
     if (!taskToEdit) {
-      return next(new AppError('there was not task with that id found', 404));
+      return next(new AppError("there was not task with that id found", 404));
     }
 
     //task is found
-    const editedTask = await editTask(taskId, taskData, req.app.get('db'));
+    const editedTask = await editTask(taskId, taskData, req.app.get("db"));
 
-    res.status(200).json({ message: 'Task edited successfully', task: editedTask });
+    res
+      .status(200)
+      .json({ message: "Task edited successfully", task: editedTask });
   } catch (error: any) {
-    if (process.env.NODE_ENV !== 'production') {
-      console.log('unable to edit task', error.message);
+    if (process.env.NODE_ENV !== "production") {
+      console.log("unable to edit task", error.message);
     }
-    return next(new AppError(error.message || 'an error occured while editing tasks', 500));
+    return next(
+      new AppError(error.message || "an error occured while editing tasks", 500)
+    );
     //TODO:log error to a logging system
   }
 }
 
-export async function markTaskAsCompleted(req: Request, res: Response, next: NextFunction) {}
+export async function getTaskBYHome(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { homeId } = req.params;
+
+  if (!homeId) {
+    return next(new AppError("invalid HomeId provided", 400));
+  }
+
+  try {
+    const fetchedTask: Task[] = await findTaskByGroupHome(
+      Number(homeId),
+      req.app.get("db")
+    );
+
+    if (!fetchedTask || fetchedTask.length === 0) {
+      return next(new AppError("No task found at the moment", 404));
+    }
+
+    res.status(200).json({
+      message: "Tasks retrieved successfully",
+      tasks: fetchedTask,
+    });
+  } catch (error: any) {
+    if (process.env.NODE_ENV !== "production") {
+      console.log("unable to edit task", error.message);
+    }
+    return next(
+      new AppError(error.message || "an error occured while getting tasks", 500)
+    );
+    //TODO:log error to a logging system
+  }
+}
+
+export async function markTaskAsCompleted(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {}
--- routes/taskRoutes.ts
+++ routes/taskRoutes.ts
@@ -1,10 +1,15 @@
-import express from 'express';
-import checkAuth, { checkRole } from '../utils/checkAuth.js';
-import { addTaskData, editTaskData, markTaskAsCompleted } from '../controller/taskController.js';
+import express from "express";
+import checkAuth, { checkRole } from "../utils/checkAuth.js";
+import {
+  addTaskData,
+  editTaskData,
+  getTaskBYHome,
+} from "../controller/taskController.js";
 
 const router = express.Router();
 
-router.post('/add-task', checkAuth, checkRole, addTaskData);
-router.post('/edit-task/:id', checkAuth, editTaskData);
+router.post("/add-task", checkAuth, checkRole, addTaskData);
+router.post("/edit-task/:id", checkAuth, editTaskData);
+router.get("/all-GroupHome-task/:homeId", checkAuth, getTaskBYHome);
 
 export default router;
