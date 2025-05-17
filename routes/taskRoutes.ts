419800aeb24ca4e61d2292cd247a302ab4ee5217
Alain Descartes <ualaindescartes@gmail.com>
2025-05-16 20:48:08 -0600

    add a check role

--- routes/taskRoutes.ts
+++ routes/taskRoutes.ts
@@ -1,17 +1,17 @@
-import express from "express";
-import checkAuth, { checkRole } from "../utils/checkAuth.js";
+import express from 'express';
+import checkAuth, { checkRole } from '../utils/checkAuth.js';
 import {
   addTaskData,
   deleteTaskById,
   editTaskData,
   getTaskBYHome,
-} from "../controller/taskController.js";
+} from '../controller/taskController.js';
 
 const router = express.Router();
 
-router.post("/add-task", checkAuth, checkRole, addTaskData);
-router.post("/edit-task/:id", checkAuth, editTaskData);
-router.get("/all-GroupHome-task/:homeId", checkAuth, getTaskBYHome);
-router.delete("/delete-task/:id", checkRole, checkAuth, deleteTaskById);
+router.post('/add-task', checkAuth, checkRole, addTaskData);
+router.post('/edit-task/:id', checkAuth, checkRole, editTaskData);
+router.get('/all-GroupHome-task/:homeId', checkAuth, getTaskBYHome);
+router.delete('/delete-task/:id', checkRole, checkAuth, deleteTaskById);
 
 export default router;
