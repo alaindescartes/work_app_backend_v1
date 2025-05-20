bf9fe248af715172b1cde91f68f22660dd4684f4
Alain Descartes <ualaindescartes@gmail.com>
2025-05-19 20:20:17 -0600

    added docker and made some corrections

--- controller/authController.ts
+++ controller/authController.ts
@@ -1,9 +1,10 @@
 import { AppError } from '../utils/appError.js';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { Session } from 'express-session';
 import bcrypt from 'bcrypt';
 import { getStaffData } from '../models/staffModel.js';
 
-interface StaffSession {
+interface StaffSession extends Session {
   staff?: {
     staffId?: number;
     firstName: string;
@@ -15,11 +16,8 @@ interface StaffSession {
     email: string;
   };
 }
-export async function login(
-  req: Request & { session: StaffSession },
-  res: Response,
-  next: NextFunction
-) {
+export const login: RequestHandler = async (req, res, next) => {
+  const session = req.session as unknown as StaffSession;
   try {
     const { email, password } = req.body;
     if (!email || !password) {
@@ -35,25 +33,22 @@ export async function login(
       return next(new AppError('invalid credentials', 401));
     }
     const { password: _, ...other } = staffData;
-    req.session.staff = other;
-    return res.status(200).json({
+    session.staff = other;
+    res.status(200).json({
       message: 'user signed in successfully',
-      staffData: req.session.staff,
+      staffData: session.staff,
     });
   } catch (error: any) {
     return next(new AppError(error.message || 'could not sign in admin', 400));
   }
-}
+};
 
-export function logout(
-  req: Request & { session: StaffSession },
-  res: Response,
-  next: NextFunction
-) {
-  if (!req.session.staff) {
+export const logout: RequestHandler = (req, res, next) => {
+  const session = req.session as StaffSession;
+  if (!session.staff) {
     return next(new AppError('user not logged in', 401));
   }
-  req.session.destroy((err) => {
+  session.destroy((err) => {
     if (err) {
       return next(new AppError('could not log out user', 400));
     }
@@ -62,22 +57,19 @@ export function logout(
       message: 'user logged out successfully',
     });
   });
-}
+};
 
 // TODO:fix the check auth code(getting a type mismatch error)
-export function getSession(
-  req: Request & { session: StaffSession },
-  res: Response,
-  next: NextFunction
-) {
+export const getSession: RequestHandler = (req, res, next) => {
+  const session = req.session as unknown as StaffSession;
   try {
-    const session = req.session as any;
     if (!session?.staff) {
       console.log('session expired :', session?.staff);
-      return res.status(401).json({ message: 'Session expired' });
+      res.status(401).json({ message: 'Session expired' });
+      return;
     }
-    return res.status(200).json({ user: session.staff });
+    res.status(200).json({ user: session.staff });
   } catch (error) {
     next(new AppError('cannot check auth', 500));
   }
-}
+};
