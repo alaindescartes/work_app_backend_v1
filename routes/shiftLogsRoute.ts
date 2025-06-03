31b180c7f6eda32690ca3c0257ea421bf77c32d7
Alain Descartes <ualaindescartes@gmail.com>
2025-06-03 12:56:38 -0600

    renamed route

--- routes/shiftLogsRoute.ts
+++ routes/shiftLogsRoute.ts
@@ -3,7 +3,7 @@ import checkAuth from '../utils/checkAuth.js';
 import { addLog, getLogById, getLogs } from '../controller/shiftLogsController.js';
 const router = express.Router();
 router.get('/logs/:homeId', checkAuth, getLogs);
-router.get('/logs/:logId', checkAuth, getLogById);
+router.get('/get-logs-id/:logId', checkAuth, getLogById);
 router.post('/add-logs', checkAuth, addLog);
 
 export default router;
