9e184f11db00777d90216df243e59382fff9070a
alain descartes <ualaindescartes@gmail.com>
2025-06-02 23:29:25 -0600

    added route to get client finance summary

--- routes/financeRoute.ts
+++ routes/financeRoute.ts
@@ -5,11 +5,13 @@ import {
   addCashCount,
   cashTransaction,
   getCashCountByHome,
+  getDetailedClientFinanceSummary,
 } from '../controller/financeController.js';
 const router = express.Router();
 router.get('/get-cashCount-by-home/:homeId', checkAuth, getCashCountByHome);
 router.post('/add-cashCount', checkAuth, addCashCount);
 router.post('/add-allowance/:supervisorId', checkAuth, addAllowance);
 router.post('/cash-transaction', checkAuth, cashTransaction);
+router.get('/client-financial-summary/:resId', checkAuth, getDetailedClientFinanceSummary);
 
 export default router;
